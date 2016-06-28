f9e30e4724d38d5e1982c48881edbf537de86715
Kyle Clemens <jkc.clemens@gmail.com>
2016-06-28 15:03:32 -0400

    feat: add -f and -r to install.zsh
    -f forces symlinks, moving existing files to <name>_old. -r removes _old files.

--- install.zsh
+++ install.zsh
@@ -1,9 +1,30 @@
+force="false"
+remove_old="false"
+while getopts fr opt; do
+  case $opt in
+    f)
+      force="true"
+      ;;
+    r)
+      remove_old="true"
+  esac
+done
+
 for file in `ls files/*`; do
   name=`basename $file`
-  if [[ -e ~/.$name ]]; then
+  if "$remove_old" == "true"; then
+    if [[ -e ~/.${name}_old ]]; then
+      rm -f ~/.${name}_old
+      echo "Removed ~/.${name}_old."
+    fi
+  elif [[ -e ~/.$name && "$force" != "true" ]]; then
     echo "~/.$name already existed, so no action was taken."
   else
-    ln -s "$(realpath $file)" "$(realpath -m ~/.$name)"
+    if [[ -e ~/.$name ]]; then
+      mv ~/.$name ~/.${name}_old
+      echo "Moved ~/.$name to ~/.${name}_old."
+    fi
+    ln -s $(realpath $file) $(realpath -m ~/.$name)
     echo "$file symlinked to ~/.$name"
   fi
 done
