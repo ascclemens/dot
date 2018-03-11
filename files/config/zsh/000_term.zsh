e8f0f55e3a34879e745a5add8554cab9d83aa273
Kyle Clemens <me@kyleclemens.com>
2018-03-11 03:47:21 -0400

    fix: use screen in tmux

--- files/config/zsh/000_term.zsh
+++ files/config/zsh/000_term.zsh
@@ -4,4 +4,10 @@
 
 # Yes, everything about this is bad. However, it's basically the only TERM that gets everyone to
 # play together nicely. iTerm -> tmux -> ssh/mosh -> tmux maintaining proper keybinds is a miracle.
-export TERM=screen-256color
+# So the way this works is that we use xterm-256color everywhere EXCEPT tmux. tmux requires
+# screen-256color.
+if [[ $TMUX != "" ]]; then
+  export TERM=screen-256color
+else
+  export TERM=xterm-256color
+fi
