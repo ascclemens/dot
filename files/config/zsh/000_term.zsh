4340d8ef43fb466f1f91fb54186a0663c0eb4818
Kyle Clemens <me@kyleclemens.com>
2018-03-10 22:31:42 -0500

    feat: set term

---
+++ files/config/zsh/000_term.zsh
@@ -0,0 +1,3 @@
+# Yes, everything about this is bad. However, it's basically the only TERM that gets everyone to
+# play together nicely. iTerm -> tmux -> ssh/mosh -> tmux maintaining proper keybinds is a miracle.
+export TERM=screen-256color
