589c61411691b21e9bbad69eb80214b7c41abb68
Kyle Clemens <me@kyleclemens.com>
2018-03-11 00:44:33 -0500

    chore: add header

--- files/config/zsh/000_term.zsh
+++ files/config/zsh/000_term.zsh
@@ -1,3 +1,7 @@
+########
+# TERM #
+########
+
 # Yes, everything about this is bad. However, it's basically the only TERM that gets everyone to
 # play together nicely. iTerm -> tmux -> ssh/mosh -> tmux maintaining proper keybinds is a miracle.
 export TERM=screen-256color
