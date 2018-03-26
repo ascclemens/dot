57d3d80c6ad8bfb29cf9d1d7ede79d9df252f9cf
Kyle Clemens <git@kyleclemens.com>
2018-03-26 09:58:07 -0400

    fix: check if binaries exist before aliasing

--- zsh/.config/zsh/107_aliases.zsh
+++ zsh/.config/zsh/107_aliases.zsh
@@ -2,14 +2,17 @@
 # Aliases #
 ###########
 
-alias battery="pmset -g batt"
-alias bytes="gnumfmt --to=iec-i --suffix=B"
-[[ -e $(which hub) ]] && alias git=hub
-alias ka="k -A"
+[[ -x $(which pmset) ]] && alias battery="pmset -g batt"
+[[ -x $(which numfmt) ]] && alias bytes="numfmt --to=iec-i --suffix=B"
+[[ -x $(which gnumfmt) ]] && alias bytes="gnumfmt --to=iec-i --suffix=B"
+[[ -x $(which hub) ]] && alias git=hub
+[[ -x $(which mpsyt) ]] && alias mpsyt='mpsyt; reset_title'
+[[ -x $(which tmuxinator) ]] && alias mux=tmuxinator
+
+if [[ -x /Applications/VLC.app/Contents/MacOS/VLC && -x $(which reattach-to-user-namespace) ]]; then
+  alias vlc="reattach-to-user-namespace /Applications/VLC.app/Contents/MacOS/VLC"
+fi
+
 alias ls="ls -Gp"
 alias lsa="ls -A"
-alias mpsyt='mpsyt; reset_title'
-alias mux=tmuxinator
-alias pseudo=sudo
 alias reset_title='printf "\e]0;$(hostname)\a"'
-alias vlc="reattach-to-user-namespace /Applications/VLC.app/Contents/MacOS/VLC"
