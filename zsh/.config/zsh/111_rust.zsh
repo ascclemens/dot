9c47cd988946fe8de0c1e016aea2c5b02259a085
Kyle Clemens <git@kyleclemens.com>
2018-03-26 09:58:51 -0400

    fix: check if rust exists

--- zsh/.config/zsh/111_rust.zsh
+++ zsh/.config/zsh/111_rust.zsh
@@ -2,5 +2,9 @@
 # Rust #
 ########
 
-export PATH=$PATH:~/.cargo/bin
-export RUST_SRC_PATH=~/.multirust/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
+if [[ -d ~/.cargo/bin ]]; then
+  export PATH=$PATH:~/.cargo/bin
+fi
+if [[ -d ~/.multirust/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src ]]; then
+  export RUST_SRC_PATH=~/.multirust/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
+fi
