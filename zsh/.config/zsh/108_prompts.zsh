187d6c6404de34b566b7750f3431e22cd2b1d488
Kyle Clemens <git@kyleclemens.com>
2018-03-26 22:37:11 -0400

    fix: don't assume git-freq

--- zsh/.config/zsh/108_prompts.zsh
+++ zsh/.config/zsh/108_prompts.zsh
@@ -26,7 +26,7 @@ function git_status {
 }
 
 function git_spark {
-  if [[ "x$(git status 2> /dev/null)" != "x" ]]; then
+  if [[ -x $(which git-freq) && $(git status 2>/dev/null) != "" ]]; then
     echo " $(git freq $(date '+%Y-%m-%d') -7 | spark)"
   fi
 }
