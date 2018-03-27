861d4fbdda64cbc6565f01bed66539b9f5d476cc
Kyle Clemens <git@kyleclemens.com>
2018-03-27 00:59:25 -0400

    feat: add init.sh

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # .dot
 
-Clone to `~/.dot`, use [GNU Stow](https://www.gnu.org/software/stow/) to initialize whichever packages you want.
+Clone to `~/.dot`, use [GNU Stow](https://www.gnu.org/software/stow/) to initialize whichever
+packages you want.
 
 ```sh
 stow zsh
 stow path
 # and so on
 ```
+
+## init.sh
+
+Also included is a quick shell script I wrote (compatible with even bare `sh`, assuming it supports
+`local`) to bootstrap new servers.
+
+It assumes Debian-based (e.g. has `apt`). It kind of also assumes `curl` is installed, assuming you
+run it with `curl -sSf ... | sh`, but otherwise not.
+
+It will update `apt`, upgrade, upgrade your distribution if necessary, install some packages, set
+your default shell to `zsh`, download this repo, install a program to generate `$PROMPT` and
+`$RPROMPT`, install a program to clean and sort `$PATH`, run `stow zsh` and `stow path`, then prompt
+to install [Rust](https://rust-lang.org/).
+
+It does not pollute the shell it is run in.
---
+++ init.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+
+__box_init() {
+  set -e
+
+  local nopasswd=0
+  local darwin=0
+
+  if [ "$(uname)" = 'Darwin' ]; then
+    darwin=1
+  else
+    darwin=0
+  fi
+
+  if [ -x "$(command -v sudo)" ]; then
+    local cap=''
+    cap="$(sudo -l)"
+    if echo "$cap" | grep NOPASSWD; then
+      nopasswd=1
+      local sudo="sudo"
+    elif echo "$cap" | grep ALL; then
+      local sudo="sudo"
+    else
+      echo 'not enough sudo privileges'
+      echo 'cowardly refusing to continue'
+    fi
+  elif [ "$(whoami)" = "root" ]; then
+    local sudo=""
+  else
+    echo 'no sudo found and not root'
+    echo 'cowardly refusing to continue'
+    return
+  fi
+
+  echo 'updating apt...'
+  $sudo apt-get update
+
+  echo 'upgrading system...'
+  $sudo apt-get -y upgrade && $sudo apt-get -y dist-upgrade
+
+  echo 'installing zsh, curl, git, xz-utils, and stow...'
+  $sudo apt-get install -y zsh curl git xz-utils stow
+
+  echo 'setting shell to zsh...'
+  if [ $nopasswd = 1 ]; then
+    # use sudo since it will work and won't prompt for a password
+    if [ $darwin = 1 ]; then
+      $sudo chsh -s "$(command -v zsh)" -u "$(whoami)"
+    else
+      $sudo chsh -s "$(command -v zsh)" "$(whoami)"
+    fi
+  else
+    # no point in using sudo, as it will require the same password
+    chsh -s "$(which zsh)"
+  fi
+
+  echo 'downloading dot files...'
+  git clone https://github.com/jkcclemens/dot.git ~/.dot
+
+  echo 'moving to tempdir...'
+  local temp=''
+  temp="$(mktemp -d)"
+  cd "$temp"
+
+  echo 'making ~/.prompt...'
+  mkdir -p ~/.prompt
+
+  echo 'making ~/.local/bin...'
+  mkdir -p ~/.local/bin
+
+  echo 'downloading path and prompt...'
+  curl https://kyleclemens.com/assets/path_and_prompt.tar.xz > path_and_prompt.tar.xz
+
+  echo 'extracting...'
+  tar xvf path_and_prompt.tar.xz
+
+  echo 'moving binaries...'
+  mv prompt ~/.prompt/prompt
+  mv path ~/.local/bin/path
+
+  echo 'moving to ~/.dot...'
+  cd ~/.dot
+
+  echo 'removing tempdir...'
+  rm -rfv "$temp"
+
+  echo 'making ~/.config/zsh...'
+  mkdir -p ~/.config/zsh
+
+  echo 'stowing zsh and path...'
+  stow zsh
+  stow path
+
+  printf 'install rust? [y/N] '
+  read -r install
+  if [ "$install" = 'y' ] || [ "$install" = 'Y' ]; then
+    curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly --no-modify-path -y
+  fi
+
+  echo 'all done. log out and back in for zsh.'
+}
+
+__box_init
+
+if command -v unfunction >/dev/null 2>&1; then
+  unfunction __box_init
+elif command -v unset >/dev/null 2>&1; then
+  unset __box_init
+fi
