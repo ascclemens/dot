7da02dcb718637b2202352a3d8f36374609c77b2
Kyle Clemens <git@kyleclemens.com>
2018-03-28 11:31:54 -0400

    fix: quote keychain eval

--- zsh/.config/zsh/115_keychain.zsh
+++ zsh/.config/zsh/115_keychain.zsh
@@ -3,4 +3,4 @@
 ############
 
 # Start keychain as our ssh-agent, using AddKeysToAgent in ~/.ssh/config
-[[ -x $(which keychain) ]] && eval $(keychain --eval --quiet --noask --agents ssh)
+[[ -x $(which keychain) ]] && eval "$(keychain --eval --quiet --noask --agents ssh)"
