fb927ddaaa0a96255ef0059f7de0da7a69d08370
Kyle Clemens <git@kyleclemens.com>
2018-03-28 10:15:42 -0400

    refactor: handle brew prefixes better

--- zsh/.config/zsh/101_brew.zsh
+++ zsh/.config/zsh/101_brew.zsh
@@ -3,6 +3,18 @@
 ########
 
 if [[ -x $(which brew) ]]; then
-  export OPENSSL_INCLUDE_DIR="$(brew --prefix openssl)/include"
-  export OPENSSL_LIB_DIR="$(brew --prefix openssl)/lib"
+  function __do_brew() {
+    local openssl_prefix
+    if openssl_prefix=$(brew --prefix openssl); then
+      export OPENSSL_INCLUDE_DIR="$openssl_prefix/include"
+      export OPENSSL_LIB_DIR="$openssl_prefix/lib"
+    fi
+
+    local curl_prefix
+    if curl_prefix=$(brew --prefix curl); then
+      export PATH="$curl_prefix/bin:$PATH"
+    fi
+  }
+  __do_brew
+  unfunction __do_brew
 fi
