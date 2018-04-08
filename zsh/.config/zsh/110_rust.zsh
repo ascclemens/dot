0baf69663c05cdd1f81dbfa2e6ca1be9b7711dab
Kyle Clemens <git@kyleclemens.com>
2018-04-08 13:44:36 -0400

    fix: set RUST_SRC_PATH correctly on non-Darwin

--- zsh/.config/zsh/110_rust.zsh
+++ zsh/.config/zsh/110_rust.zsh
@@ -5,6 +5,12 @@
 if [[ -d ~/.cargo/bin ]]; then
   export PATH=$PATH:~/.cargo/bin
 fi
-if [[ -d ~/.multirust/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src ]]; then
-  export RUST_SRC_PATH=~/.multirust/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
+if [[ $(uname) == "Darwin" ]]; then
+  if [[ -d ~/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src ]]; then
+    export RUST_SRC_PATH=~/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
+  fi
+else
+  if [[ -d ~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src ]]; then
+    export RUST_SRC_PATH=~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
+  fi
 fi
