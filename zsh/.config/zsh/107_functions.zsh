1444f32aefb779bb0b0e441a51804fbb107d940d
Kyle Clemens <git@kyleclemens.com>
2018-08-22 08:35:06 -0400

    feat: allow specifying region for ec2

--- zsh/.config/zsh/107_functions.zsh
+++ zsh/.config/zsh/107_functions.zsh
@@ -68,17 +68,17 @@ function ec2_ssh() {
   local ec2_ip
   local curr_ip
 
-  echo "Getting current SSH IP in $1"
-  ec2_ip=$(aws ec2 describe-security-groups --group-name "$1" | jq -r '.SecurityGroups[0].IpPermissions | map(select(.FromPort == 22)) | .[0].IpRanges[0].CidrIp')
+  echo "Getting current SSH IP in $2"
+  ec2_ip=$(aws --region "$1" ec2 describe-security-groups --group-name "$2" | jq -r '.SecurityGroups[0].IpPermissions | map(select(.FromPort == 22)) | .[0].IpRanges[0].CidrIp')
 
   echo "Getting current IP"
   curr_ip="$(curl -4sSfL icanhazip.com | tr -d '\n')/32"
 
-  echo "Revoking $ec2_ip in $1"
-  aws ec2 revoke-security-group-ingress --region us-east-1 --group-name "$1" --protocol tcp --port 22 --cidr "$ec2_ip"
+  echo "Revoking $ec2_ip in $2"
+  aws ec2 revoke-security-group-ingress --region "$1" --group-name "$2" --protocol tcp --port 22 --cidr "$ec2_ip"
 
-  echo "Authorizing $curr_ip in $1"
-  aws ec2 authorize-security-group-ingress --region us-east-1 --group-name "$1" --protocol tcp --port 22 --cidr "$curr_ip"
+  echo "Authorizing $curr_ip in $2"
+  aws ec2 authorize-security-group-ingress --region "$1" --group-name "$2" --protocol tcp --port 22 --cidr "$curr_ip"
 
   echo "Done"
 }
