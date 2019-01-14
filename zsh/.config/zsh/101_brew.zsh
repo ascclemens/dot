f9d43ce33b4a64bc945a3ef57eac29c3ad681958
Kyle Clemens <git@kyleclemens.com>
2019-01-14 08:39:54 -0500

    fix: only set brew paths if necessary

--- zsh/.config/zsh/101_brew.zsh
+++ zsh/.config/zsh/101_brew.zsh
@@ -4,21 +4,27 @@
 
 if [[ -x $(command -v brew) ]]; then
   function __do_brew() {
-    local openssl_prefix
-    if openssl_prefix=$(brew --prefix openssl); then
-      export OPENSSL_INCLUDE_DIR="$openssl_prefix/include"
-      export OPENSSL_LIB_DIR="$openssl_prefix/lib"
+    if brew ls --versions openssl > /dev/null; then
+      local openssl_prefix
+      if openssl_prefix=$(brew --prefix openssl); then
+        export OPENSSL_INCLUDE_DIR="$openssl_prefix/include"
+        export OPENSSL_LIB_DIR="$openssl_prefix/lib"
+      fi
     fi
 
-    local curl_prefix
-    if curl_prefix=$(brew --prefix curl); then
-      export PATH="$curl_prefix/bin:$PATH"
+    if brew ls --versions curl > /dev/null; then
+      local curl_prefix
+      if curl_prefix=$(brew --prefix curl); then
+        export PATH="$curl_prefix/bin:$PATH"
+      fi
     fi
 
-    local site_functions
-    site_functions=$(brew --prefix)/share/zsh/site-functions
-    if [[ -d $site_functions ]]; then
-      FPATH="$site_functions:$FPATH"
+    if brew ls --versions zsh > /dev/null; then
+      local site_functions
+      site_functions=$(brew --prefix)/share/zsh/site-functions
+      if [[ -d $site_functions ]]; then
+        FPATH="$site_functions:$FPATH"
+      fi
     fi
   }
   __do_brew
