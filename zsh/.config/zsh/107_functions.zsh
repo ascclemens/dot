978a945db244f8e53c9002093371574e37bc855e
Kyle Clemens <git@kyleclemens.com>
2019-01-21 00:50:42 -0500

    feat: add simple note management and remove transfer

--- zsh/.config/zsh/107_functions.zsh
+++ zsh/.config/zsh/107_functions.zsh
@@ -11,58 +11,6 @@ mkdircd() {
 # undollar
 \$() { $@ }
 
-#
-# Defines transfer alias and provides easy command line file and folder sharing.
-#
-# Authors:
-#   Remco Verhoef <remco@dutchcoders.io>
-#
-transfer() {
-  # check arguments
-  if [ $# -eq 0 ];
-  then
-    echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"
-    return 1
-  fi
-
-  # get temporarily filename, output is written to this file show progress can be showed
-  tmpfile=$( mktemp -t transferXXX )
-
-  # upload stdin or file
-  file=$1
-
-  if tty -s;
-  then
-    basefile=$(basename "$file" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
-
-    if [ ! -e "$file" ];
-    then
-      echo "File $file doesn't exist."
-      return 1
-    fi
-
-    if [ -d "$file" ];
-    then
-      # zip directory and transfer
-      zipfile=$( mktemp -t transferXXX.zip )
-      cd $(dirname "$file") && zip -r -q - $(basename "$file") >> "$zipfile"
-      curl --progress-bar --upload-file "$zipfile" "https://transfer.sh/$basefile.zip" >> "$tmpfile"
-      rm -f "$zipfile"
-    else
-      # transfer file
-      curl --progress-bar --upload-file "$file" "https://transfer.sh/$basefile" >> "$tmpfile"
-    fi
-  else
-    # transfer pipe
-    curl --progress-bar --upload-file "-" "https://transfer.sh/$file" >> "$tmpfile"
-  fi
-
-  # cat output link
-  cat "$tmpfile"
-
-  # cleanup
-  rm -f "$tmpfile"
-}
 
 function ec2_ssh() {
   local ec2_ip
@@ -81,4 +29,29 @@ function ec2_ssh() {
   aws ec2 authorize-security-group-ingress --region "$1" --group-name "$2" --protocol tcp --port 22 --cidr "$curr_ip"
 
   echo "Done"
+}
+
+# Simple note management
+export NOTES_PATH="$HOME/Sync/notes"
+
+function note {
+  if [[ "$NOTES_PATH" == "" ]]; then
+    echo 'Please define $NOTES_PATH.'
+    return 1
+  fi
+
+  local title="$*"
+  if [[ "$title" == "" ]]; then
+    echo "Please provide a note name."
+    return 2
+  fi
+
+  local subdir
+  subdir=$(date +'%Y-%m-%d')
+
+  local final_path="$NOTES_PATH/$subdir"
+
+  mkdir -p "$final_path"
+
+  nano "$final_path/$title.txt"
 }
