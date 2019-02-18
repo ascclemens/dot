f3ebe31f9ae67eb4d28a950f81fea35084de1cb2
Kyle Clemens <git@kyleclemens.com>
2019-02-18 08:58:30 -0500

    feat: use gpg for ssh auth

---
+++ zsh/.config/zsh/201_gpg.zsh
@@ -0,0 +1,9 @@
+#######
+# GPG #
+#######
+
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+  export SSH_AUTH_SOCK
+fi
