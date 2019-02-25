0b3a7f98d38dd4c4fb469addeffe13d29cb20533
Kyle Clemens <git@kyleclemens.com>
2019-02-25 09:54:04 -0500

    refactor: remove ec2_ssh

--- zsh/.config/zsh/107_functions.zsh
+++ zsh/.config/zsh/107_functions.zsh
@@ -10,23 +10,3 @@ mkdircd() {
 
 # undollar
 \$() { $@ }
-
-
-function ec2_ssh() {
-  local ec2_ip
-  local curr_ip
-
-  echo "Getting current SSH IP in $2"
-  ec2_ip=$(aws --region "$1" ec2 describe-security-groups --group-name "$2" | jq -r '.SecurityGroups[0].IpPermissions | map(select(.FromPort == 22)) | .[0].IpRanges[0].CidrIp')
-
-  echo "Getting current IP"
-  curr_ip="$(curl -4sSfL icanhazip.com | tr -d '\n')/32"
-
-  echo "Revoking $ec2_ip in $2"
-  aws ec2 revoke-security-group-ingress --region "$1" --group-name "$2" --protocol tcp --port 22 --cidr "$ec2_ip"
-
-  echo "Authorizing $curr_ip in $2"
-  aws ec2 authorize-security-group-ingress --region "$1" --group-name "$2" --protocol tcp --port 22 --cidr "$curr_ip"
-
-  echo "Done"
-}
