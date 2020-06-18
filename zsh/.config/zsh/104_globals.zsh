8fbffa2b54ec351d9158a0da82119927c78a9365
Anna Clemens <git@annaclemens.io>
2020-06-18 01:28:10 -0400

    feat: set editor based on available programs

--- zsh/.config/zsh/104_globals.zsh
+++ zsh/.config/zsh/104_globals.zsh
@@ -2,13 +2,6 @@
 # Globals #
 ###########
 
-# set editor based on host
-if [[ -x $(command -v code) ]]; then
-  export EDITOR="code -wn"
-else
-  export EDITOR=nano
-fi
-
 # set java home if on macOS
 [[ -x /usr/libexec/java_home ]] && export JAVA_HOME=$(/usr/libexec/java_home 2>/dev/null)
 
