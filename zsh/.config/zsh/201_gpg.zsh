fa18daa8f5e703d710f1ae74b70b7c232dbd89b0
Anna Clemens <git@annaclemens.io>
2021-01-08 12:27:09 -0500

    fix(zsh/gpg): launch gpg-agent if necessary

--- zsh/.config/zsh/201_gpg.zsh
+++ zsh/.config/zsh/201_gpg.zsh
@@ -6,4 +6,5 @@ unset SSH_AGENT_PID
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
   SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
   export SSH_AUTH_SOCK
+  gpgconf --launch gpg-agent
 fi
